6e61a1ef860d5813ec57d8ac2b982948f7d8c9f7
sayanta <sayantasingh01@gmail.com>
2023-01-18 21:52:59 +0530

    update

--- lua/user/alpha.lua
+++ lua/user/alpha.lua
@@ -24,7 +24,7 @@ dashboard.section.buttons.val = {
   dashboard.button("p", " " .. " Find project", ":lua require('telescope').extensions.projects.projects()<CR>"),
   dashboard.button("r", "󱑓 " .. " Recent files", ":Telescope oldfiles <CR>"),
   dashboard.button("t", " " .. " Find text", ":Telescope live_grep <CR>"),
-  dashboard.button("c", " " .. " Config", ":e ~/.config/nvim/init.lua <CR>"),
+  dashboard.button("c", " " .. " Config", ":e $MYVIMRC | :cd %:p:h <CR>"),
   dashboard.button("h", " " .. " Neovim Check health", ":checkhealth<CR>"),
   dashboard.button("q", " " .. " Quit", ":qa<CR>"),
 }
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -1,15 +1,15 @@
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
-    return
+	return
 end
 
 -- popup window
 packer.init({
-    display = {
-        open_fn = function()
-            return require("packer.util").float({ border = "rounded" })
-        end,
-    },
+	display = {
+		open_fn = function()
+			return require("packer.util").float({ border = "rounded" })
+		end,
+	},
 })
 
 -- Autocommand that reloads neovim whenever you save the plugins.lua file
@@ -21,92 +21,92 @@ vim.cmd([[
 ]])
 
 local ensure_packer = function()
-    local fn = vim.fn
-    local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-    if fn.empty(fn.glob(install_path)) > 0 then
-        fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
-        vim.cmd([[packadd packer.nvim]])
-        return true
-    end
-    return false
+	local fn = vim.fn
+	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+		vim.cmd([[packadd packer.nvim]])
+		return true
+	end
+	return false
 end
 
 local packer_bootstrap = ensure_packer()
 
 return require("packer").startup(function(use)
-    use("wbthomason/packer.nvim")
-    use("nvim-lua/plenary.nvim")
-    use("nvim-lua/popup.nvim")
-    use("kyazdani42/nvim-web-devicons")
-    use("antoinemadec/FixCursorHold.nvim") -- This is needed to fix lsp doc highlight
-    use("moll/vim-bbye")
-    use("lewis6991/impatient.nvim")
+	use("wbthomason/packer.nvim")
+	use("nvim-lua/plenary.nvim")
+	use("nvim-lua/popup.nvim")
+	use("kyazdani42/nvim-web-devicons")
+	use("antoinemadec/FixCursorHold.nvim") -- This is needed to fix lsp doc highlight
+	use("moll/vim-bbye")
+	use("lewis6991/impatient.nvim")
 
-    use("goolord/alpha-nvim")
-    use("numToStr/Comment.nvim")
-    use("windwp/nvim-autopairs") -- Autopairs, integrates with cmp & treesitter
-    use("nvim-lualine/lualine.nvim")
-    use("ahmedkhalf/project.nvim")
-    use("lukas-reineke/indent-blankline.nvim")
-    use("akinsho/bufferline.nvim")
-    use("akinsho/toggleterm.nvim")
-    use("kyazdani42/nvim-tree.lua")
-    use("nvim-telescope/telescope.nvim")
-    use("nvim-telescope/telescope-media-files.nvim")
-    use("folke/which-key.nvim")
-    use("ThePrimeagen/vim-be-good")
+	use("goolord/alpha-nvim")
+	use("numToStr/Comment.nvim")
+	use("windwp/nvim-autopairs") -- Autopairs, integrates with cmp & treesitter
+	use("nvim-lualine/lualine.nvim")
+	use("ahmedkhalf/project.nvim")
+	use("lukas-reineke/indent-blankline.nvim")
+	use("akinsho/bufferline.nvim")
+	use("akinsho/toggleterm.nvim")
+	use("kyazdani42/nvim-tree.lua")
+	use("nvim-telescope/telescope.nvim")
+	use("nvim-telescope/telescope-media-files.nvim")
+	use("folke/which-key.nvim")
+	use("ThePrimeagen/vim-be-good")
 
-    -- Themes
-    use("morhetz/gruvbox")
-    use("rose-pine/neovim")
-    use("LunarVim/darkplus.nvim")
-    use("LunarVim/onedarker.nvim")
-    use("folke/tokyonight.nvim")
-    use("p00f/nvim-ts-rainbow")
-    use("folke/zen-mode.nvim")
-    use("norcalli/nvim-colorizer.lua")
-    use("uga-rosa/ccc.nvim")
+	-- Themes
+	use("morhetz/gruvbox")
+	use("rose-pine/neovim")
+	use("LunarVim/darkplus.nvim")
+	use("LunarVim/onedarker.nvim")
+	use("folke/tokyonight.nvim")
+	use("p00f/nvim-ts-rainbow")
+	use("folke/zen-mode.nvim")
+	use("norcalli/nvim-colorizer.lua")
+	use("uga-rosa/ccc.nvim")
 
-    -- Cmp plugins
-    use("hrsh7th/nvim-cmp")
-    use("hrsh7th/cmp-nvim-lsp")
-    use("hrsh7th/cmp-buffer")
-    use("hrsh7th/cmp-path")
-    use("hrsh7th/cmp-nvim-lua")
-    use("saadparwaiz1/cmp_luasnip")
-    use("hrsh7th/cmp-cmdline")
-    --[[ use "f3fora/cmp-spell" ]]
+	-- Cmp plugins
+	use("hrsh7th/nvim-cmp")
+	use("hrsh7th/cmp-nvim-lsp")
+	use("hrsh7th/cmp-buffer")
+	use("hrsh7th/cmp-path")
+	use("hrsh7th/cmp-nvim-lua")
+	use("saadparwaiz1/cmp_luasnip")
+	use("hrsh7th/cmp-cmdline")
+	--[[ use "f3fora/cmp-spell" ]]
 
-    -- Snippets
-    use("L3MON4D3/LuaSnip")
-    use("rafamadriz/friendly-snippets")
+	-- Snippets
+	use("L3MON4D3/LuaSnip")
+	use("rafamadriz/friendly-snippets")
 
-    -- LSP
-    use("williamboman/mason.nvim")
-    use("williamboman/mason-lspconfig.nvim")
-    use("neovim/nvim-lspconfig")
-    use("jose-elias-alvarez/null-ls.nvim")
-    use("RRethy/vim-illuminate")
-    use("onsails/lspkind.nvim")
+	-- LSP
+	use("williamboman/mason.nvim")
+	use("williamboman/mason-lspconfig.nvim")
+	use("neovim/nvim-lspconfig")
+	use("jose-elias-alvarez/null-ls.nvim")
+	use("RRethy/vim-illuminate")
+	use("onsails/lspkind.nvim")
 
-    -- Treesitter
-    use({
-        "nvim-treesitter/nvim-treesitter",
-        run = ":TSUpdate",
-    })
-    use("JoosepAlviste/nvim-ts-context-commentstring")
+	-- Treesitter
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		run = ":TSUpdate",
+	})
+	use("JoosepAlviste/nvim-ts-context-commentstring")
 
-    -- DAP
-    use("mfussenegger/nvim-dap")
-    use("rcarriga/nvim-dap-ui")
-    use("ravenxrz/DAPInstall.nvim")
+	-- DAP
+	use("mfussenegger/nvim-dap")
+	use("rcarriga/nvim-dap-ui")
+	use("ravenxrz/DAPInstall.nvim")
 
-    -- Git
-    use("lewis6991/gitsigns.nvim")
+	-- Git
+	use("lewis6991/gitsigns.nvim")
 
-    -- Automatically set up your configuration after cloning packer.nvim
-    -- Put this at the end after all plugins
-    if packer_bootstrap then
-        require("packer").sync()
-    end
+	-- Automatically set up your configuration after cloning packer.nvim
+	-- Put this at the end after all plugins
+	if packer_bootstrap then
+		require("packer").sync()
+	end
 end)
