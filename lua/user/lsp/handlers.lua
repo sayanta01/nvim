86d52811ed439f4cfe4f12e595079462fa69a904
sayanta <sayantasingh01@gmail.com>
2023-01-21 05:15:15 +0530

    update

--- lua/user/lsp/handlers.lua
+++ lua/user/lsp/handlers.lua
@@ -20,7 +20,7 @@ local on_attach = function(client, bufnr)
 	end
 
 	local config = {
-		virtual_text = false, -- disable virtual text
+		virtual_text = true, -- disable virtual text
 		signs = {
 			active = signs, -- show signs
 		},
