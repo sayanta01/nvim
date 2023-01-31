d5466c5812b5191d2b59c4ad4a82fcdf16ac9c35
sayanta <sayantasingh01@gmail.com>
2023-01-31 09:09:07 +0530

    update

--- lua/user/colorizer.lua
+++ lua/user/colorizer.lua
@@ -9,5 +9,10 @@ local options = {
 	RRGGBBAA = true,
 }
 require("colorizer").setup({
+	"css",
+	"javascript",
 	"*",
+	"!lazy", -- Exclude vim from highlighting.
+	css = { rgb_fn = true }, -- Enable parsing rgb(...) functions in css.
+	html = { names = true }, -- Disable parsing "names" like Blue or Gray
 }, options)
