8311d5b3a301c1a9b00785b7f8085e428aa214d9
sayanta <sayantasingh01@gmail.com>
2023-02-01 08:45:27 +0530

    update

--- lua/user/colorizer.lua
+++ lua/user/colorizer.lua
@@ -1,23 +1,37 @@
-local options = {
-	RGB = true,
-	RRGGBB = true,
-	RRGGBBAA = true,
-	AARRGGBB = false, -- 0xAARRGGBB hex codes
-	names = true,
-	rgb_fn = true,
-	hsl_fn = true,
-	css = true,
-	css_fn = true,
-	--[[ virtualtext = "■", ]]
-}
 require("colorizer").setup({
-	--[[ "*",  -- Enable for all ]]
-	"html",
-	"css",
-	"javascript",
-	"lua",
-	"sh",
-	"!lazy", -- Exclude vim from highlighting.
-	css = { rgb_fn = true }, -- Enable parsing rgb(...) functions in css.
-	html = { names = true }, -- Disable parsing "names" like Blue or Gray
-}, options)
+	filetypes = {
+		--[[ "*", ]]
+		"sh",
+		"lua",
+		"astro",
+		"typescript",
+		"typescriptreact",
+		"javascript",
+		"html",
+		"css",
+		css = { rgb_fn = true }, -- Enable parsing rgb(...) functions in css.
+		html = { names = true }, -- Disable parsing "names" like Blue or Gray
+		"!prompt",
+		"!lazy", -- Exclude vim from highlighting.
+	},
+	user_default_options = {
+		RGB = true,
+		RRGGBB = true,
+		names = true,
+		RRGGBBAA = false,
+		AARRGGBB = false,
+		rgb_fn = false,
+		hsl_fn = false,
+		css = false,
+		css_fn = false,
+		-- Available modes for `mode`: foreground, background,  virtualtext
+		mode = "background", -- Set the display mode.
+		-- Available methods are false / true / "normal" / "lsp" / "both"
+		-- True is same as normal
+		tailwind = false, -- `both`
+		sass = { enable = false, parsers = { "css" } }, -- Enable sass colors
+		virtualtext = "■",
+	},
+	-- all the sub-options of filetypes apply to buftypes
+	buftypes = {},
+})
--- lua/user/mason.lua
+++ lua/user/mason.lua
@@ -1,91 +1,91 @@
-require("mason").setup()
+require("mason").setup({
+    ui = {
+        border = "rounded",
+        icons = {
+            package_installed = "◍",
+            package_pending = "◍",
+            package_uninstalled = "",
+            keymaps = {
+                toggle_package_expand = "<CR>",
+                install_package = "i",
+                update_package = "u",
+                check_package_version = "c",
+                update_all_packages = "U",
+                check_outdated_packages = "C",
+                uninstall_package = "X",
+                cancel_installation = "<C-c>",
+                apply_language_filter = "<C-f>",
+            },
+        },
+    },
+})
 
 require("mason-lspconfig").setup({
-	ensure_installed = {
-		"clangd", -- c, cpp, c#
-		"cssls",
-		"omnisharp",
-		"gopls",
-		"jdtls", -- java
-		"tailwindcss", -- javascript
-		"tsserver",
-		"cssmodules_ls",
-		"eslint",
-		"jsonls",
-		"sumneko_lua",
-		"jedi_language_server", -- python
-		"pyright",
-		"pylsp",
-		"ruby_ls",
-		"rust_analyzer",
-		"angularls", -- typescript
-		"marksman",
-		"graphql",
-		"html",
-		"emmet_ls",
-		"sqlls",
-		"dockerls",
-		"bashls",
-		"ltex",
-		"yamlls",
-		"vimls",
-	},
-	automatic_installation = true,
-
-	ui = {
-		border = "rounded",
-		icons = {
-			package_installed = "",
-			package_pending = "",
-			package_uninstalled = "",
-		},
-		keymaps = {
-			toggle_package_expand = "<CR>",
-			install_package = "i",
-			update_package = "u",
-			check_package_version = "c",
-			update_all_packages = "U",
-			check_outdated_packages = "C",
-			uninstall_package = "X",
-			cancel_installation = "<C-c>",
-			apply_language_filter = "<C-f>",
-		},
-	},
+    ensure_installed = {
+        "clangd", -- c, cpp, c#
+        "cssls",
+        "omnisharp",
+        "gopls",
+        "jdtls", -- java
+        "tailwindcss", -- javascript
+        "tsserver",
+        "cssmodules_ls",
+        "eslint",
+        "jsonls",
+        "sumneko_lua",
+        "jedi_language_server", -- python
+        "pyright",
+        "pylsp",
+        "ruby_ls",
+        "rust_analyzer",
+        "angularls", -- typescript
+        "marksman",
+        "graphql",
+        "html",
+        "emmet_ls",
+        "sqlls",
+        "dockerls",
+        "bashls",
+        "ltex",
+        "yamlls",
+        "vimls",
+    },
+    automatic_installation = true,
 })
 
 require("mason-null-ls").setup({
-	-- list of formatters & linters for mason to install
-	ensure_installed = {
-		"yamllint",
-		"clang_format",
-		"gofumpt",
-		"eslint_d",
-		"prettier",
-		"stylua",
-		"autopep8",
-		"rustfmt",
-		"markdownlint",
-		"phpcbf",
-		"shfmt",
-		"yamlfmt",
-		"rubocop",
-		--[[ "sqlfluff", ]]
-		--[[ "solhint", ]]
-	},
-	automatic_installation = true,
+    -- list of formatters & linters for mason to install
+    ensure_installed = {
+        "yamllint",
+        "clang_format",
+        "gofumpt",
+        "eslint_d",
+        "prettier",
+        "stylua",
+        "autopep8",
+        "rustfmt",
+        "markdownlint",
+        "phpcbf",
+        "shfmt",
+        "yamlfmt",
+        "rubocop",
+        --[[ "sqlfluff", ]]
+        --[[ "solhint", ]]
+    },
+    automatic_installation = true,
 })
 
 require("mason-nvim-dap").setup({
-	--[[ debugger ]]
-	ensure_installed = {
-		"delve",
-		"python",
-		"bash",
-		"javadbg",
-		"node2",
-		"chrome",
-		"firefox",
-		"php",
-	},
-	automatic_installation = true,
+    --[[ debugger ]]
+    ensure_installed = {
+        "delve",
+        "python",
+        "bash",
+        "javadbg",
+        "node2",
+        "chrome",
+        "firefox",
+        "php",
+    },
+    automatic_installation = true,
 })
