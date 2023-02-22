6d62b949e412f472f257bf74d0637690eed40934
sayanta <sayantasingh01@gmail.com>
2023-02-23 04:36:45 +0530

    update

--- lua/user/alpha.lua
+++ lua/user/alpha.lua
@@ -19,13 +19,13 @@ dashboard.section.header.val = {
 }
 dashboard.section.buttons.val = {
 	dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
-	dashboard.button("e", "  New file", ":ene | startinsert <CR>"),
+	dashboard.button("n", "  New file", ":ene | startinsert <CR>"),
 	dashboard.button("b", "  Bookmarks", ":Telescope marks<CR>"),
 	dashboard.button("p", "  Projects", ":lua require('telescope').extensions.projects.projects()<CR>"),
 	dashboard.button("r", "󱑓  Recent files", ":Telescope oldfiles <CR>"),
 	dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
-	dashboard.button("s", "  Config", ":e $MYVIMRC | :cd %:p:h <CR>"),
-	dashboard.button("h", "  Neovim check health", ":checkhealth<CR>"),
+	dashboard.button("c", "  Config", ":e $MYVIMRC | :cd %:p:h <CR>"),
+	dashboard.button("h", "  Check health", ":checkhealth<CR>"),
 	dashboard.button("q", "  quit", ":qa<CR>"),
 }
 local function footer()
