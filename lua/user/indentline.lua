6f508af91ab526ec2f3763f9b6213a6560ecc848
sayanta <sayantasingh01@gmail.com>
2023-02-25 02:25:52 +0530

    update

--- lua/user/indentline.lua
+++ lua/user/indentline.lua
@@ -44,7 +44,8 @@ require("indent_blankline").setup({
 	char = "",
 	show_trailing_blankline_indent = false,
 	show_first_indent_level = true,
-	use_treesitter = true,
 	--[[ context_char = "▏", ]]
-	show_current_context_start = false,
+	--[[ use_treesitter = true, ]]
+	--[[ show_current_context = true, ]]
+	--[[ show_current_context_start = true, ]]
 })
