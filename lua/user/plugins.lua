7441f11b0b914ec0a2377dfd0a95bfe300234668
sayanta <sayantasingh01@gmail.com>
2023-03-21 16:05:27 +0530

    update

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -72,6 +72,26 @@ require("lazy").setup({
 	{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
 	"windwp/nvim-autopairs", -- Autopairs, integrates with cmp & treesitter
 	"lukas-reineke/indent-blankline.nvim",
+	--[[ { ]]
+	--[[ 	"echasnovski/mini.indentscope", ]]
+	--[[ 	version = false, -- wait till new 0.7.0 release to put it back on semver ]]
+	--[[ 	event = { "BufReadPre", "BufNewFile" }, ]]
+	--[[ 	opts = { ]]
+	--[[ 		symbol = "", ]]
+	--[[ 		options = { try_as_border = true }, ]]
+	--[[ 	}, ]]
+	--[[ 	init = function() ]]
+	--[[ 		vim.api.nvim_create_autocmd("FileType", { ]]
+	--[[ 			pattern = { "help", "alpha", "lazy", "NvimTree", "text" }, ]]
+	--[[ 			callback = function() ]]
+	--[[ 				vim.b.miniindentscope_disable = true ]]
+	--[[ 			end, ]]
+	--[[ 		}) ]]
+	--[[ 	end, ]]
+	--[[ 	config = function(_, opts) ]]
+	--[[ 		require("mini.indentscope").setup(opts) ]]
+	--[[ 	end, ]]
+	--[[ }, ]]
 	{
 		"JoosepAlviste/nvim-ts-context-commentstring",
 		event = "VeryLazy",
