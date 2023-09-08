82ff5aee59343015a60f618a422e29d704e713f3
sayanta <sayantasingh01@gmail.com>
2023-09-08 11:48:52 +0530

    update

--- lua/user/settings.lua
+++ lua/user/settings.lua
@@ -50,53 +50,53 @@ opt.updatetime = 260
 opt.completeopt = { "menuone", "noselect" }
 
 -- Disable nvim intro
-opt.spelllang:append("cjk") -- disable spellchecking for asian characters (VIM algorithm does not support)
-opt.shortmess:append("c") -- don't show redundant messages from ins-completion-menu
-opt.shortmess:append("sI")
-opt.whichwrap:append("<,>,[,],h,l")
+opt.spelllang:append "cjk"  -- disable spellchecking for asian characters (VIM algorithm does not support it)
+opt.shortmess:append "c"
+opt.whichwrap:append "<,>,[,],h,l"
 
 -- Disable builtin plugins
 local disabled_built_ins = {
-	--[[ "netrw", ]]
-	--[[ "netrwPlugin", ]]
-	"netrwSettings",
-	"netrwFileHandlers",
-	"gzip",
-	"tar",
-	"tarPlugin",
-	"zip",
-	"zipPlugin",
-	"getscript",
-	"getscriptPlugin",
-	"vimball",
-	"vimballPlugin",
-	"2html_plugin",
-	"spellfile_plugin",
-	"rplugin",
-	"matchit",
-	"rrhelper",
-	"syntax",
-	"synmenu",
-	"optwin",
-	"compiler",
-	"bugreport",
-	"ftplugin",
-	"tutor",
+  --[[ "netrw", ]]
+  --[[ "netrwPlugin", ]]
+  "netrwSettings",
+  "netrwFileHandlers",
+  "gzip",
+  "tar",
+  "tarPlugin",
+  "zip",
+  "zipPlugin",
+  "getscript",
+  "getscriptPlugin",
+  "vimball",
+  "vimballPlugin",
+  "2html_plugin",
+  "spellfile_plugin",
+  "rplugin",
+  "matchit",
+  "rrhelper",
+  "syntax",
+  "synmenu",
+  "optwin",
+  "compiler",
+  "bugreport",
+  "ftplugin",
+  "tutor",
   "tohtml",
-	--[[ "logipat", ]]
+  "lazyredraw",
+  --[[ "logipat", ]]
 }
 
 for _, plugin in pairs(disabled_built_ins) do
-	g["loaded_" .. plugin] = 1
+  g["loaded_" .. plugin] = 1
 end
 
 local default_providers = {
-	"node",
-	"perl",
-	"python3",
-	"ruby",
+  "node",
+  "perl",
+  "python3",
+  "ruby",
 }
 
 for _, provider in ipairs(default_providers) do
-	vim.g["loaded_" .. provider .. "_provider"] = 0
+  vim.g["loaded_" .. provider .. "_provider"] = 0
 end
