2f3f67f7909191f60cbe99513912eb8e3f1bd1ae
sayanta <sayantasingh01@gmail.com>
2023-09-13 21:37:55 +0530

    update

--- lua/user/dap.lua
+++ lua/user/dap.lua
@@ -13,33 +13,56 @@ dapui.setup()
 vim.fn.sign_define("DapBreakpoint", { text = "", texthl = "DiagnosticSignError", linehl = "", numhl = "" })
 
 --[[ javascript ]]
-dap.adapters.node2 = {
-	type = "executable",
-	command = "node-debug2-adapter",
-	args = { os.getenv("HOME") .. "/dev/microsoft/vscode-node-debug2/out/src/nodeDebug.js" },
+dap.adapters["pwa-node"] = {
+	type = "server",
+	host = "localhost",
+	--[[ port = "${port}", ]]
+	port = 8124,
+	executable = {
+		command = "js-debug-adapter",
+	},
 }
 
-dap.configurations.javascript = {
-	{
-		name = "Launch",
-		type = "node2",
-		request = "launch",
-		program = "${file}",
-		cwd = vim.fn.getcwd(),
-		sourceMaps = true,
-		protocol = "inspector",
-		console = "integratedTerminal",
-	},
-	{
-		-- For this to work you need to make sure the node process is started with the `--inspect` flag
-		name = "Attach to process",
-		type = "node2",
-		request = "attach",
-		--[[ restart = true, ]]
-		port = 9229,
-		processId = require("dap.utils").pick_process,
-	},
-}
+for _, language in ipairs({ "typescript", "javascript" }) do
+	dap.configurations[language] = {
+		{
+			type = "pwa-node",
+			request = "launch",
+			name = "Launch file",
+			program = "${file}",
+			cwd = "${workspaceFolder}",
+			--[[ runtimeExecutable = "node", ]]
+		},
+	}
+end
+
+--[[ dap.adapters.node2 = { ]]
+--[[ 	type = "executable", ]]
+--[[ 	command = "node-debug2-adapter", ]]
+--[[ 	args = { os.getenv("HOME") .. "/dev/microsoft/vscode-node-debug2/out/src/nodeDebug.js" }, ]]
+--[[ } ]]
+
+--[[ dap.configurations.javascript = { ]]
+--[[ 	{ ]]
+--[[ 		name = "Launch", ]]
+--[[ 		type = "node2", ]]
+--[[ 		request = "launch", ]]
+--[[ 		program = "${file}", ]]
+--[[ 		cwd = vim.fn.getcwd(), ]]
+--[[ 		sourceMaps = true, ]]
+--[[ 		protocol = "inspector", ]]
+--[[ 		console = "integratedTerminal", ]]
+--[[ 	}, ]]
+--[[ 	{ ]]
+--[[ 		-- For this to work you need to make sure the node process is started with the `--inspect` flag ]]
+--[[ 		name = "Attach to process", ]]
+--[[ 		type = "node2", ]]
+--[[ 		request = "attach", ]]
+--[[     --	restart = true, ]]
+--[[ 		port = 9229, ]]
+--[[ 		processId = require("dap.utils").pick_process, ]]
+--[[ 	}, ]]
+--[[ } ]]
 
 dap.listeners.after.event_initialized["dapui_config"] = function()
 	dapui.open()
