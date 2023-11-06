97921bfea7c7b60d7b0607159edfab5daa8726fa
sayanta <sayantasingh01@gmail.com>
2023-11-06 15:02:23 +0530

    update

--- lua/user/plugins/noice.lua
+++ lua/user/plugins/noice.lua
@@ -99,7 +99,7 @@ return {
 					-- override the lsp markdown formatter with Noice
 					["vim.lsp.util.stylize_markdown"] = true,
 					-- override cmp documentation with Noice (needs the other options to work)
-					["cmp.entry.get_documentation"] = false,
+					["cmp.entry.get_documentation"] = true,
 				},
 				hover = {
 					enabled = true,
