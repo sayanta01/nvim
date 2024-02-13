97956df972b195a0d4e9ac307cedf625301c89b6
sayanta <sayantasingh01@gmail.com>
2024-02-14 00:20:55 +0530

    update

--- lua/user/plugins/treesitter.lua
+++ lua/user/plugins/treesitter.lua
@@ -53,13 +53,14 @@ return {
 
 					-- disable = { "latex" },
 					-- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
-					-- disable = function(lang, buf)
-					-- 	local max_filesize = 100 * 1024 -- 100 KB
-					-- 	local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-					-- 	if ok and stats and stats.size > max_filesize then
-					-- 		return true
-					-- 	end
-					-- end,
+					disable = function(_, buf)
+						local max_filesize = 100 * 1024 -- 100 KB
+						local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+						if ok and stats and stats.size > max_filesize then
+							vim.notify("Treesitter disabled")
+							return true
+						end
+					end,
 
 					additional_vim_regex_highlighting = false,
 				},
