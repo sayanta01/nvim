0e4349990d6071d5e5c64da195e80d7bf1984a65
sayanta <sayantasingh01@gmail.com>
2024-02-24 00:15:00 +0530

    update

--- lua/user/plugins/treesitter.lua
+++ lua/user/plugins/treesitter.lua
@@ -55,7 +55,6 @@ return {
 						local max_filesize = 100 * 1024 -- 100 KB
 						local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
 						if ok and stats and stats.size > max_filesize then
-							vim.notify("Treesitter disabled")
 							return true
 						end
 					end,
