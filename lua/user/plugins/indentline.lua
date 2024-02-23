3bc83a884388eb8fc312b19107039e96b7501c07
sayanta <sayantasingh01@gmail.com>
2024-02-23 19:59:47 +0530

    update

--- lua/user/plugins/indentline.lua
+++ lua/user/plugins/indentline.lua
@@ -1,29 +1,27 @@
 return {
 	"lukas-reineke/indent-blankline.nvim",
 	main = "ibl",
-	event = { "BufReadPost", "BufNewFile" },
-	config = function()
-		local highlight = {
-			-- "CursorColumn",
-			"Whitespace",
-		}
-		require("ibl").setup({
-			-- ▏ ┆ ╎ : ∘
-			indent = { highlight = highlight, char = "" },
-			whitespace = {
-				highlight = highlight,
-				remove_blankline_trail = false,
+	event = { "InsertEnter" },
+	opts = {
+		-- ▏ ┆ ╎ : ∘
+		indent = {
+			char = "▏",
+			tab_char = "▏",
+		},
+		whitespace = {
+			remove_blankline_trail = false,
+		},
+		scope = { enabled = true, show_start = false },
+		exclude = {
+			filetypes = {
+				"help",
+				"alpha",
+				"neo-tree",
+				"lazy",
+				"mason",
+				"notify",
+				"toggleterm",
 			},
-			scope = { enabled = true, show_start = false },
-			exclude = {
-				filetypes = {
-					"help",
-					"alpha",
-					"lazy",
-					"mason",
-					"toggleterm",
-				},
-			},
-		})
-	end,
+		},
+	},
 }
