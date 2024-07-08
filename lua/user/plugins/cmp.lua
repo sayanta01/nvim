90f48fe855e5189fce18a7b2659d80ddb9d58848
sayanta <sayantasingh01@gmail.com>
2024-07-08 15:50:15 +0530

    update

--- lua/user/plugins/cmp.lua
+++ lua/user/plugins/cmp.lua
@@ -116,7 +116,7 @@ return {
 				["<Tab>"] = cmp.mapping(function(fallback)
 					if cmp.visible() then
 						cmp.select_next_item()
-					elseif luasnip.expand_or_locally_jumpable() then
+					elseif require("luasnip").expand_or_jumpable() then
 						luasnip.expand_or_jump()
 					elseif has_words_before() then
 						cmp.complete()
