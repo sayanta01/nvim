4ab979ebb884d25f16dcb0490a5553590dc95ef1
sayanta <sayantasingh01@gmail.com>
2024-07-16 11:07:24 +0530

    update

--- lua/user/plugins/surround.lua
+++ lua/user/plugins/surround.lua
@@ -1,10 +1,10 @@
 return {
 	"kylechui/nvim-surround",
 	version = "*",
-	keys = { "cs", "ds", "ys" }, -- ys<w> new word
+	keys = { "ys", "ds", "cs" }, -- ysiw" new word
 	config = function()
 		require("nvim-surround").setup({
-			-- use [{( left parentheses adds space
+			-- use {[( left parentheses adds space
 			keymaps = {
 				insert = "<C-g>s",
 				insert_line = "<C-g>S",
@@ -15,7 +15,7 @@ return {
 				visual = "S",
 				visual_line = "gS",
 				delete = "ds",
-				change = "cs", -- cst <change tag> & dst <delete tag>
+				change = "cs", -- use t for <tag>
 				change_line = "cS",
 			},
 		})
