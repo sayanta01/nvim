62b6453adca534e7969bf45af179fe2d65ba27b8
sayanta <sayantasingh01@gmail.com>
2024-08-09 14:54:20 +0530

    update

--- lua/user/plugins/lsp/lsp-config.lua
+++ lua/user/plugins/lsp/lsp-config.lua
@@ -25,11 +25,8 @@ return {
 			vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
 			vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
 			vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-			vim.keymap.set("n", "gl", vim.diagnostic.open_float, opts)
 			vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
 			vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
-			-- vim.keymap.set("n", "<space>lf", vim.lsp.buf.format, opts)
-			-- vim.keymap.set("n", "<space>lq", vim.diagnostic.setloclist, opts)
 		end
 
 		local capabilities = vim.lsp.protocol.make_client_capabilities()
--- lua/user/plugins/which-key.lua
+++ lua/user/plugins/which-key.lua
@@ -83,7 +83,6 @@ return {
         { "<leader>gC", ":Telescope git_bcommits<cr>",                                       desc = "Commits (current file)", },
 
         { "<leader>l",  group = "LSP",                                                       mode = { "n", "v" } },
-        { "<leader>ld", ":lua vim.lsp.buf.declaration()<cr>",                                desc = "Goto Declaration", },
         { "<leader>lh", ":lua vim.lsp.buf.signature_help()<cr>",                             desc = "Signature Help", },
         { "<leader>lt", ":lua vim.lsp.buf.type_definition()<cr>",                            desc = "Type Definition", },
         { "<leader>lr", ":lua vim.lsp.buf.rename()<cr>",                                     desc = "Rename", },
@@ -92,6 +91,7 @@ return {
         { "<leader>lW", ":lua vim.lsp.buf.remove_workspace_folder()<cr>",                    desc = "Remove Workspace Folder", },
         { "<leader>ll", ":lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<cr>", desc = "List Workspace Folder", },
         { "<leader>lf", ":lua vim.lsp.buf.format()<CR>",                                     desc = "Format", },
+        { "<leader>ld", ":lua vim.diagnostic.open_float()<cr>",                              desc = "Line Diagnostics", },
         { "<leader>lq", ":lua vim.diagnostic.setloclist()<cr>",                              desc = "Quickfix", },
         { "<leader>ls", ":Telescope lsp_document_symbols<cr>",                               desc = "Document Symbols", },
         { "<leader>lS", ":Telescope lsp_dynamic_workspace_symbols<cr>",                      desc = "Workspace Symbols", },
