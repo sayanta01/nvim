dccb5c3f0329fe1d07d106395e5741a155b7e43e
sayanta01 <sayantasingh01@gmail.com>
2024-11-15 02:29:08 +0530

    update

--- lua/plugins/java.lua
+++ lua/plugins/java.lua
@@ -1,46 +1,65 @@
 return {
-	"mfussenegger/nvim-jdtls",
-	ft = "java",
-	config = function()
-		local status, jdtls = pcall(require, "jdtls")
-		if not status then
-			return
-		end
+  "mfussenegger/nvim-jdtls",
+  ft = "java",
+  config = function()
+    local jdtls = require("jdtls")
+    local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
+    local home = os.getenv('HOME')
+    local workspace_dir = home .. "/.cache/jdtls/workspace" .. project_name
+    local jdtls_path = require("mason-registry").get_package("jdtls"):get_install_path()
+    local launcher_jar = vim.fn.glob(jdtls_path .. "/plugins/org.eclipse.equinox.launcher_*.jar")
+    local sysname = vim.fn.system('uname -s'):gsub("\n", ""):lower()
+    local config_dir = jdtls_path .. "/config_" .. sysname
+    local lombok_path = jdtls_path .. "/lombok.jar"
 
-		local config = {
-			cmd = { vim.fn.expand("~/.local/share/nvim/mason/bin/jdtls") },
-			root_dir = vim.fs.dirname(vim.fs.find({ "gradlew", ".git", "mvnw" }, { upward = true })[1]),
-			init_options = {
-				bundles = {
-					vim.fn.glob(
-						"~/.local/share/nvim/mason/packages/java-debug-adapter/extension/server/com.microsoft.java.debug.plugin-*.jar",
-						1
-					),
-				},
-			},
-		}
+    local config = {
+      cmd = {
+        'java',
+        '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+        '-Dosgi.bundles.defaultStartLevel=4',
+        '-Declipse.product=org.eclipse.jdt.ls.core.product',
+        '-Dlog.protocol=true',
+        '-Dlog.level=ALL',
+        '-javaagent:' .. lombok_path,
+        '-Xmx1g',
+        '--add-modules=ALL-SYSTEM',
+        '--add-opens', 'java.base/java.util=ALL-UNNAMED',
+        '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
+        '-jar', launcher_jar,
+        '-configuration', config_dir,
+        '-data', workspace_dir,
+      },
 
-		config["on_attach"] = function(client, bufnr)
-			local _, _ = pcall(vim.lsp.codelens.refresh)
-			require("jdtls.dap").setup_dap_main_class_configs()
-			jdtls.setup_dap({ hotcodereplace = "auto" })
-			require("user.plugins.lsp.lspconfig").on_attach(client, bufnr)
-		end
+      root_dir = require('jdtls.setup').find_root({ ".git", "mvnw", "gradlew", "pom.xml", "build.gradle" }),
+      init_options = {
+        bundles = {
+          vim.fn.glob(home ..
+            "~/.local/share/nvim/mason/packages/java-debug-adapter/extension/server/com.microsoft.java.debug.plugin-*.jar",
+            true
+          ),
+        },
+      },
+    }
 
-		require("jdtls").start_or_attach(config)
+    config["on_attach"] = function(client, bufnr)
+      local _, _ = pcall(vim.lsp.codelens.refresh)
+      jdtls.setup_dap({ hotcodereplace = "auto" })
+      require("jdtls.dap").setup_dap_main_class_configs()
+      require("plugins.lsp.lsp-config").on_attach(client, bufnr)
+    end
 
-		local keymap = vim.keymap.set
-		local opts = { silent = true }
+    require("jdtls").start_or_attach(config)
 
-		keymap("n", "<leader>ji", ":lua require'jdtls'.organize_imports()<CR>", opts)
-		keymap("n", "<leader>jv", ":lua require('jdtls').extract_variable()<CR>", opts)
-		keymap("v", "<leader>jv", "<Esc><Cmd>lua require('jdtls').extract_variable(true)<CR>", opts)
-		keymap("n", "<leader>jc", ":lua require('jdtls').extract_constant()<CR>", opts)
-		keymap("v", "<leader>jc", "<Esc><Cmd>lua require('jdtls').extract_constant(true)<CR>", opts)
-		keymap("v", "<leader>jm", "<Esc><Cmd>lua require('jdtls').extract_method(true)<CR>", opts)
-
-		-- Dap
-		keymap("n", "<leader>jt", ":lua require'jdtls'.test_class()<CR>", opts)
-		keymap("n", "<leader>jn", ":lua require'jdtls'.test_nearest_method()<CR>", opts)
-	end,
+    local keymap = vim.keymap.set
+    local opts = { noremap = true, silent = true }
+    keymap("n", "<leader>ji", ":lua require'jdtls'.organize_imports()<CR>", opts)
+    keymap("n", "<leader>jv", ":lua require('jdtls').extract_variable()<CR>", opts)
+    keymap("v", "<leader>jv", "<Esc><Cmd>lua require('jdtls').extract_variable(true)<CR>", opts)
+    keymap("n", "<leader>jc", ":lua require('jdtls').extract_constant()<CR>", opts)
+    keymap("v", "<leader>jc", "<Esc><Cmd>lua require('jdtls').extract_constant(true)<CR>", opts)
+    keymap("v", "<leader>jm", "<Esc><Cmd>lua require('jdtls').extract_method(true)<CR>", opts)
+    -- Dap
+    keymap("n", "<leader>jt", ":lua require'jdtls'.test_class()<CR>", opts)
+    keymap("n", "<leader>jn", ":lua require'jdtls'.test_nearest_method()<CR>", opts)
+  end,
 }
