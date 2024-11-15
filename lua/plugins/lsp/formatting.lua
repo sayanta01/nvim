11c9040ff00b5f74db2287aa52ced43acc1062db
sayanta01 <sayantasingh01@gmail.com>
2024-11-16 01:13:16 +0530

    update

--- lua/plugins/lsp/formatting.lua
+++ lua/plugins/lsp/formatting.lua
@@ -1,69 +1,69 @@
 return {
-  "stevearc/conform.nvim",
-  event = "BufReadPost",
-  cmd = "ConformInfo",
-  keys = {
-    {
-      "<leader>lf",
-      function()
-        require("conform").format({ async = true, lsp_fallback = true })
-      end,
-      mode = { "n", "v" },
-      desc = "Format buffer",
-    },
-  },
-  dependencies = {
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-    config = function()
-      local mason_tool_installer = require("mason-tool-installer")
-      mason_tool_installer.setup({
-        ensure_installed = {
-          -- "debugpy",
-          -- "codelldb",
-          -- "delve",
-          -- "js-debug-adapter",
-          -- "java-debug-adapter",
-          "prettier",
-          "shfmt",
-          "shellcheck", -- bashls automatically uses
-          "stylua",
-          "isort",
-          "black",
-          -- "pylint",
-          -- "eslint",
-        },
-      })
-    end,
-  },
-  opts = {
-    formatters_by_ft = {
-      html = { "prettier" },
-      css = { "prettier" },
-      scss = { "prettier" },
-      javascript = { "prettier" },
-      typescript = { "prettier" },
-      javascriptreact = { "prettier" },
-      typescriptreact = { "prettier" },
-      vue = { "prettier" },
-      svelte = { "prettier" },
-      graphql = { "prettier" },
-      json = { "prettier" },
-      jsonc = { "prettier" },
-      yaml = { "prettier" },
-      markdown = { "prettier" },
-      java = { "google-java-format" },
-      go = { "gofmt" },
-      c = { "clang_format" },
-      cpp = { "clang_format" },
-      sh = { "shfmt" },
-      lua = { "stylua" },
-      python = { "isort", "black" },
-      -- terraform = { "terraform_fmt" },
-    },
-    -- format_on_save = {
-    -- 	timeout_ms = 500,
-    -- 	async = true,
-    -- 	lsp_fallback = true,
-    -- },
-  },
+	-- "stevearc/conform.nvim",
+	-- event = "BufReadPost",
+	-- cmd = "ConformInfo",
+	-- keys = {
+	--   {
+	--     "<leader>lf",
+	--     function()
+	--       require("conform").format({ async = true, lsp_fallback = true })
+	--     end,
+	--     mode = { "n", "v" },
+	--     desc = "Format buffer",
+	--   },
+	-- },
+	-- dependencies = {
+	--   "WhoIsSethDaniel/mason-tool-installer.nvim",
+	--   config = function()
+	--     local mason_tool_installer = require("mason-tool-installer")
+	--     mason_tool_installer.setup({
+	--       ensure_installed = {
+	--         -- "debugpy",
+	--         -- "codelldb",
+	--         -- "delve",
+	--         -- "js-debug-adapter",
+	--         -- "java-debug-adapter",
+	--         "prettier",
+	--         "shfmt",
+	--         "shellcheck", -- bashls automatically uses
+	--         "stylua",
+	--         "isort",
+	--         "black",
+	--         -- "pylint",
+	--         -- "eslint",
+	--       },
+	--     })
+	--   end,
+	-- },
+	-- opts = {
+	--   formatters_by_ft = {
+	--     html = { "prettier" },
+	--     css = { "prettier" },
+	--     scss = { "prettier" },
+	--     javascript = { "prettier" },
+	--     typescript = { "prettier" },
+	--     javascriptreact = { "prettier" },
+	--     typescriptreact = { "prettier" },
+	--     vue = { "prettier" },
+	--     svelte = { "prettier" },
+	--     graphql = { "prettier" },
+	--     json = { "prettier" },
+	--     jsonc = { "prettier" },
+	--     yaml = { "prettier" },
+	--     markdown = { "prettier" },
+	--     java = { "google-java-format" },
+	--     go = { "gofmt" },
+	--     c = { "clang_format" },
+	--     cpp = { "clang_format" },
+	--     sh = { "shfmt" },
+	--     lua = { "stylua" },
+	--     python = { "isort", "black" },
+	--     -- terraform = { "terraform_fmt" },
+	--   },
+	--   -- format_on_save = {
+	--   -- 	timeout_ms = 500,
+	--   -- 	async = true,
+	--   -- 	lsp_fallback = true,
+	--   -- },
+	-- },
 }
--- lua/plugins/lsp/none-ls.lua
+++ lua/plugins/lsp/none-ls.lua
@@ -1,73 +1,67 @@
 return {
-  -- "nvimtools/none-ls.nvim",
-  -- event = "BufReadPost",
-  -- dependencies = {
-  -- 	"nvim-lua/plenary.nvim",
-  -- 	"jay-babu/mason-null-ls.nvim",
-  -- },
-  -- keys = {
-  -- 	{ "<leader>pn", ":NullLsInfo<cr>", desc = "NullLs" },
-  -- },
-  -- config = function()
-  -- 	local null_ls = require("null-ls")
-  -- 	local null_ls_utils = require("null-ls.utils")
-  --
-  -- 	local mason_null_ls = require("mason-null-ls")
-  -- 	mason_null_ls.setup({
-  -- 		ensure_installed = {
-  -- 			"prettier",
-  -- 			"stylua",
-  -- 			"shfmt",
-  -- 			"shellcheck", -- bashls automatically uses
-  -- 			"isort",
-  -- 			"black",
-  -- 			-- "pylint",
-  -- 			-- "eslint",
-  -- 		},
-  -- 	})
-  --
-  -- 	local formatting = null_ls.builtins.formatting
-  -- 	-- local diagnostics = null_ls.builtins.diagnostics
-  --
-  -- 	-- setup Format on Save
-  -- 	-- local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-  --
-  -- 	null_ls.setup({
-  -- 		-- add package.json as identifier for root (for typescript monorepos)
-  -- 		root_dir = null_ls_utils.root_pattern(".null-ls-root", "Makefile", ".git", "package.json"),
-  -- 		sources = {
-  -- 			formatting.google_java_format,
-  -- 			formatting.gofmt,
-  -- 			formatting.prettier.with({
-  -- 				extra_filetypes = { "svelte" },
-  -- 			}),
-  -- 			formatting.clang_format,
-  -- 			formatting.stylua,
-  -- 			formatting.shfmt,
-  -- 			formatting.isort,
-  -- 			formatting.black,
-  -- 			-- diagnostics.pylint,
-  -- 		},
-  --
-  -- 		-- configure Format on Save
-  -- 		-- on_attach = function(current_client, bufnr)
-  -- 		-- 	if current_client.supports_method("textDocument/formatting") then
-  -- 		-- 		vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-  -- 		-- 		vim.api.nvim_create_autocmd("BufWritePre", {
-  -- 		-- 			group = augroup,
-  -- 		-- 			buffer = bufnr,
-  -- 		-- 			callback = function()
-  -- 		-- 				vim.lsp.buf.format({
-  -- 		-- 					filter = function(client)
-  -- 		-- 						-- only use null-ls for formatting instead of lsp server
-  -- 		-- 						return client.name == "null-ls"
-  -- 		-- 					end,
-  -- 		-- 					bufnr = bufnr,
-  -- 		-- 				})
-  -- 		-- 			end,
-  -- 		-- 		})
-  -- 		-- 	end
-  -- 		-- end,
-  -- 	})
-  -- end,
+	"nvimtools/none-ls.nvim",
+	event = "BufReadPost",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"jay-babu/mason-null-ls.nvim",
+	},
+	keys = {
+		{ "<leader>pn", ":NullLsInfo<cr>", desc = "Nulls" },
+	},
+	config = function()
+		local null_ls = require("null-ls")
+		local mason_null_ls = require("mason-null-ls")
+		mason_null_ls.setup({
+			ensure_installed = {
+				"prettier",
+				"stylua",
+				"shellcheck", -- bashls automatically uses
+				"shfmt",
+				"isort",
+				"black",
+				-- "pylint",
+				-- "eslint",
+			},
+		})
+
+		local formatting = null_ls.builtins.formatting
+		-- local diagnostics = null_ls.builtins.diagnostics
+		-- local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+		null_ls.setup({
+			root_dir = require("null-ls.utils").root_pattern(".null-ls-root", "Makefile", ".git", "package.json"),
+			sources = {
+				formatting.prettier.with({
+					extra_filetypes = { "svelte" },
+				}),
+				formatting.google_java_format,
+				formatting.gofmt,
+				formatting.clang_format,
+				formatting.shfmt,
+				formatting.stylua,
+				formatting.isort,
+				formatting.black,
+				-- diagnostics.pylint,
+			},
+
+			-- Format on save
+			-- on_attach = function(current_client, bufnr)
+			-- 	if current_client.supports_method("textDocument/formatting") then
+			-- 		vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			-- 		vim.api.nvim_create_autocmd("BufWritePre", {
+			-- 			group = augroup,
+			-- 			buffer = bufnr,
+			-- 			callback = function()
+			-- 				vim.lsp.buf.format({
+			-- 					bufnr = bufnr,
+			-- 					filter = function(client)
+			-- 						return client.name == "null-ls"
+			-- 					end,
+			-- 				})
+			-- 			end,
+			-- 		})
+			-- 	end
+			-- end,
+		})
+	end,
 }
