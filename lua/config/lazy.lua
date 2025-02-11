60320491d4b9ba930f6cb2b3ef6f965e3c48bf19
sayanta <sayantasingh01@gmail.com>
2025-02-11 18:58:00 +0530

    update

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,81 +1,81 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-	if vim.v.shell_error ~= 0 then
-		vim.api.nvim_echo({
-			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-			{ out, "WarningMsg" },
-			{ "\nPress any key to exit..." },
-		}, true, {})
-		vim.fn.getchar()
-		os.exit(1)
-	end
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out,                            "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-	spec = {
-		{ import = "plugins" },
-		{ import = "plugins.lsp" },
-	},
-	rocks = {
-		hererocks = true,
-	},
-	change_detection = {
-		enabled = false,
-		notify = false,
-	},
-	ui = {
-		border = "rounded",
-		icons = {
-			cmd = "",
-			config = "",
-			favorite = "",
-			ft = "",
-			init = "",
-			import = "",
-			keys = "",
-			source = "",
-			start = "",
-			task = "",
-			list = {
-				"●",
-				"›",
-				"",
-				"",
-			},
-		},
-	},
-	performance = {
-		rtp = {
-			disabled_plugins = {
-				"gzip",
-				"matchit",
-				"tar",
-				"tarPlugin",
-				"tohtml",
-				"tutor",
-				"zip",
-				"zipPlugin",
-				"2html_plugin",
-				"getscript",
-				"getscriptPlugin",
-				"logipat",
-				"netrwSettings",
-				"netrwFileHandlers",
-				"rrhelper",
-				"spellfile_plugin",
-				"vimball",
-				"vimballPlugin",
-				"rplugin",
-				"syntax",
-				"synmenu",
-				"optwin",
-				"compiler",
-				"bugreport",
-				"ftplugin",
-			},
-		},
-	},
+  spec = {
+    { import = "plugins" },
+    { import = "plugins.lsp" },
+  },
+  rocks = {
+    hererocks = true,
+  },
+  change_detection = {
+    enabled = false,
+    notify = false,
+  },
+  ui = {
+    border = "rounded",
+    icons = {
+      cmd = "",
+      config = "",
+      favorite = "",
+      ft = "",
+      init = "",
+      import = "",
+      keys = "",
+      source = "",
+      start = "",
+      task = "",
+      list = {
+        "●",
+        "›",
+        "",
+        "",
+      },
+    },
+  },
+  performance = {
+    rtp = {
+      disabled_plugins = {
+        "gzip",
+        "matchit",
+        "tar",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "zip",
+        "zipPlugin",
+        "2html_plugin",
+        "getscript",
+        "getscriptPlugin",
+        "logipat",
+        "netrwSettings",
+        "netrwFileHandlers",
+        "rrhelper",
+        "spellfile_plugin",
+        "vimball",
+        "vimballPlugin",
+        "rplugin",
+        "syntax",
+        "synmenu",
+        "optwin",
+        "compiler",
+        "bugreport",
+        "ftplugin",
+      },
+    },
+  },
 })
--- lua/plugins/xtras.lua
+++ lua/plugins/xtras.lua
@@ -1,67 +1,67 @@
 return {
-  -- {
-  -- 	"folke/snacks.nvim",
-  -- 	priority = 1000,
-  -- 	lazy = false,
-  -- 	opts = {
-  -- 		bigfile = { enabled = true },
-  -- 	},
-  -- },
+	-- {
+	-- 	"folke/snacks.nvim",
+	-- 	priority = 1000,
+	-- 	lazy = false,
+	-- 	opts = {
+	-- 		bigfile = { enabled = true },
+	-- 	},
+	-- },
 
-  -- {
-  -- 	"Exafunction/codeium.nvim",
-  -- 	enabled = true,
-  -- 	build = ":Codeium Auth",
-  -- 	config = true,
-  -- },
+	-- {
+	--   "Exafunction/codeium.nvim",
+	--   enabled = false,
+	--   build = ":Codeium Auth",
+	--   config = true,
+	-- },
 
-  {
-    "monaqa/dial.nvim",
+	{
+		"monaqa/dial.nvim",
     -- stylua: ignore
     keys = {
       { "<C-a>", function() return require("dial.map").inc_normal() end, expr = true, desc = "Up",   mode = { "n", "v" } },
       { "<C-x>", function() return require("dial.map").dec_normal() end, expr = true, desc = "Down", mode = { "n", "v" } },
     },
-    config = function()
-      local augend = require("dial.augend")
-      require("dial.config").augends:register_group({
-        default = {
-          augend.integer.alias.decimal,
-          augend.integer.alias.hex,
-          augend.date.alias["%Y/%m/%d"],
-          augend.constant.alias.bool,
-          augend.semver.alias.semver,
-          augend.constant.new({ elements = { "let", "const" } }),
-        },
-      })
-    end,
-  },
+		config = function()
+			local augend = require("dial.augend")
+			require("dial.config").augends:register_group({
+				default = {
+					augend.integer.alias.decimal,
+					augend.integer.alias.hex,
+					augend.date.alias["%Y/%m/%d"],
+					augend.constant.alias.bool,
+					augend.semver.alias.semver,
+					augend.constant.new({ elements = { "let", "const" } }),
+				},
+			})
+		end,
+	},
 
-  {
-    "b0o/SchemaStore.nvim",
-    version = false,
-    lazy = true,
-  },
+	{
+		"b0o/SchemaStore.nvim",
+		version = false,
+		lazy = true,
+	},
 
-  {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    build = function()
-      require("lazy").load({ plugins = { "markdown-preview.nvim" } })
-      vim.fn["mkdp#util#install"]()
-    end,
-    keys = { { "<leader>oo", ft = "markdown", ":MarkdownPreviewToggle<CR>", desc = "Markdown Preview" } },
-    config = function()
-      vim.cmd([[do FileType]])
-    end,
-  },
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		build = function()
+			require("lazy").load({ plugins = { "markdown-preview.nvim" } })
+			vim.fn["mkdp#util#install"]()
+		end,
+		keys = { { "<leader>oo", ft = "markdown", ":MarkdownPreviewToggle<CR>", desc = "Markdown Preview" } },
+		config = function()
+			vim.cmd([[do FileType]])
+		end,
+	},
 
-  {
-    "christoomey/vim-tmux-navigator",
-    event = function()
-      if vim.fn.exists("$TMUX") == 1 then
-        return "VeryLazy"
-      end
-    end,
-  },
+	{
+		"christoomey/vim-tmux-navigator",
+		event = function()
+			if vim.fn.exists("$TMUX") == 1 then
+				return "VeryLazy"
+			end
+		end,
+	},
 }
