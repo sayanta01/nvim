4dbbcca3d826966f574cb04f762d3bcc2d9cf2bf
sayanta <sayantasingh01@gmail.com>
2025-02-18 04:10:45 +0530

    update

--- lua/plugins/lsp/lsp-config.lua
+++ lua/plugins/lsp/lsp-config.lua
@@ -25,7 +25,7 @@ return {
       vim.keymap.set("n", "gy", vim.lsp.buf.type_definition, opts)
       vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts) -- cr
       vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
-      -- vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+      vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
       vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
       vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
     end
