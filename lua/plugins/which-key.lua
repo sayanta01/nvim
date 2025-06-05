3eec62d95472f325369a0fd3d0c5ffff6382e8f0
sayanta <sayantasingh01@gmail.com>
2025-06-05 22:23:41 +0530

    update

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,23 +1,23 @@
 return {
-  "folke/which-key.nvim",
-  event = "VeryLazy",
-  init = function()
-    vim.o.timeoutlen = 300
-  end,
-  opts = {
-    preset = "helix",
-    filter = function(mapping)
-      return mapping.desc and mapping.desc ~= ""
-    end,
-    triggers = { { "<leader>", mode = { "n", "v" } } },
-    win = { title = false },
-    icons = {
-      separator = "•",
-      group = "",
-      mappings = false,
-    },
-    show_help = false,
-    show_keys = false,
+	"folke/which-key.nvim",
+	event = "VeryLazy",
+	init = function()
+		vim.o.timeoutlen = 300
+	end,
+	opts = {
+		preset = "helix",
+		filter = function(mapping)
+			return mapping.desc and mapping.desc ~= ""
+		end,
+		triggers = { { "<leader>", mode = { "n", "v" } } },
+		win = { title = false },
+		icons = {
+			separator = "•",
+			group = "",
+			mappings = false,
+		},
+		show_help = false,
+		show_keys = false,
     -- stylua: ignore
     spec = {
       { "<leader>q",  ":q!<cr>",                                                           desc = "Quit" },
@@ -27,14 +27,14 @@ return {
       { "<leader>g",  group = "git",                                                       mode = { "n", "v" } },
 
       { "<leader>c",  group = "code",                                                      mode = { "n", "v" } },
-      { "<leader>cw", ":lua vim.lsp.buf.add_workspace_folder()<cr>",                       desc = "Add Workspace Folder" },
-      { "<leader>cW", ":lua vim.lsp.buf.remove_workspace_folder()<cr>",                    desc = "Remove Workspace Folder" },
-      { "<leader>cl", ":lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<cr>", desc = "List Workspace Folder" },
+      { "<leader>ca", ":lua vim.lsp.buf.code_action()<cr>",                                desc = "Code Action" },
+      { "<leader>cr", ":lua vim.lsp.buf.rename()<cr>",                                     desc = "Rename" },
       { "<leader>cf", ":lua vim.lsp.buf.format()<CR>",                                     desc = "Format" },
       { "<leader>cd", ":lua vim.diagnostic.open_float()<cr>",                              desc = "Line Diagnostics" },
       { "<leader>cq", ":lua vim.diagnostic.setloclist()<cr>",                              desc = "Quickfix List" },
-      { "<leader>cr", ":lua vim.lsp.buf.rename()<cr>",                                     desc = "Rename" },
-      { "<leader>ca", ":lua vim.lsp.buf.code_action()<cr>",                                desc = "Code Action" },
+      -- { "<leader>cw", ":lua vim.lsp.buf.add_workspace_folder()<cr>",                       desc = "Add Workspace Folder" },
+      -- { "<leader>cW", ":lua vim.lsp.buf.remove_workspace_folder()<cr>",                    desc = "Remove Workspace Folder" },
+      -- { "<leader>cl", ":lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<cr>", desc = "List Workspace Folder" },
 
       { "<leader>u",  group = "ui", },
       { "<leader>ul", ":terminal live-server<cr>",                                         desc = "Live Server" },
@@ -49,5 +49,5 @@ return {
       { "<leader>ss", function() Snacks.picker.lsp_symbols() end,                          desc = "Symbols" },
       { "<leader>sS", function() Snacks.picker.lsp_workspace_symbols() end,                desc = "Workspace Symbols" },
     },
-  },
+	},
 }
