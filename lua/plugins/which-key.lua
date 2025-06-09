da8735f50f7271c30a5b1c058bcd708901a4ec05
sayanta <sayantasingh01@gmail.com>
2025-06-10 02:42:43 +0530

    update

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -32,9 +32,6 @@ return {
       { "<leader>cf", ":lua vim.lsp.buf.format()<CR>",                      desc = "Format" },
       { "<leader>cd", ":lua vim.diagnostic.open_float()<cr>",               desc = "Line Diagnostics" },
       { "<leader>cq", ":lua vim.diagnostic.setloclist()<cr>",               desc = "Quickfix List" },
-      -- { "<leader>cw", ":lua vim.lsp.buf.add_workspace_folder()<cr>",                       desc = "Add Workspace Folder" },
-      -- { "<leader>cW", ":lua vim.lsp.buf.remove_workspace_folder()<cr>",                    desc = "Remove Workspace Folder" },
-      -- { "<leader>cl", ":lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<cr>", desc = "List Workspace Folder" },
 
       { "<leader>u",  group = "ui", },
       { "<leader>ul", ":terminal live-server<cr>",                          desc = "Live Server" },
--- lua/plugins/xtras.lua
+++ lua/plugins/xtras.lua
@@ -1,38 +1,38 @@
 return {
-	-- {
-	-- 	"monaqa/dial.nvim",
-	--    -- stylua: ignore
-	--    keys = {
-	--      { "<C-a>",  function() return require("dial.map").inc_normal() end, expr = true, mode = { "n" } },
-	--      { "<C-x>",  function() return require("dial.map").dec_normal() end, expr = true, mode = { "n" } },
-	--    },
-	-- 	config = function()
-	-- 		local augend = require("dial.augend")
-	-- 		require("dial.config").augends:register_group({
-	-- 			default = {
-	-- 				augend.integer.alias.decimal,
-	-- 				augend.integer.alias.hex,
-	-- 				augend.date.alias["%Y/%m/%d"],
-	-- 				augend.constant.alias.bool,
-	-- 				augend.constant.new({ elements = { "let", "const" } }),
-	-- 				augend.semver.alias.semver,
-	-- 			},
-	-- 		})
-	-- 	end,
-	-- },
+  {
+    "monaqa/dial.nvim",
+    -- stylua: ignore
+    keys = {
+      { "<C-a>", function() return require("dial.map").inc_normal() end, expr = true, mode = { "n" } },
+      { "<C-x>", function() return require("dial.map").dec_normal() end, expr = true, mode = { "n" } },
+    },
+    config = function()
+      local augend = require("dial.augend")
+      require("dial.config").augends:register_group({
+        default = {
+          augend.integer.alias.decimal,
+          augend.integer.alias.hex,
+          augend.date.alias["%Y/%m/%d"],
+          augend.constant.alias.bool,
+          augend.constant.new({ elements = { "let", "const" } }),
+          augend.semver.alias.semver,
+        },
+      })
+    end,
+  },
 
-	{
-		"b0o/SchemaStore.nvim",
-		lazy = true,
-		version = false,
-	},
+  {
+    "b0o/SchemaStore.nvim",
+    lazy = true,
+    version = false,
+  },
 
-	{
-		"christoomey/vim-tmux-navigator",
-		event = function()
-			if vim.fn.exists("$TMUX") == 1 then
-				return "VeryLazy"
-			end
-		end,
-	},
+  {
+    "christoomey/vim-tmux-navigator",
+    event = function()
+      if vim.fn.exists("$TMUX") == 1 then
+        return "VeryLazy"
+      end
+    end,
+  },
 }
